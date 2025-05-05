ff348f28383ab2bc326e2e8c77bbcbb0f92b3bf3
Jayesh Hyalij <jhyalij940@gmail.com>
2025-05-05 17:59:02 +0530

    Initial commit without sensitive data

---
+++ src/main/java/kycupload/KycUploadApplication.java
@@ -0,0 +1,12 @@
+package com.example.kycupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KycUploadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KycUploadApplication.class, args);
+    }
+}
